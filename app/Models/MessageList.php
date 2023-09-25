183d97626e8cc30a18f93646176167c403ecb7e9
Mathias Hansen <me@codemonkey.io>
2023-09-25 19:35:41 +0000

    fix: Databas relationship

--- app/Models/MessageList.php
+++ app/Models/MessageList.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class MessageList extends Model
 {
     public function subscribers() {
-        return $this->belongsToMany(Subscriber::class);
+        return $this->belongsToMany(Subscriber::class, 'list_subscriber');
     }
 
     public function messages() {
